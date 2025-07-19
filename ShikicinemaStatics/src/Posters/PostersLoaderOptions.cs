88195be0144b432f24baf890d1f0bab5f2130a12
Vitaliy <31983783+BasicEC@users.noreply.github.com>
2025-07-19 16:49:08 +0300

    Small improvements (#3)
    - Add validation to PostersLoaderOptions
    - Add use CreateSlimBuilder

--- ShikicinemaStatics/src/Posters/PostersLoaderOptions.cs
+++ ShikicinemaStatics/src/Posters/PostersLoaderOptions.cs
@@ -10,7 +10,7 @@ public class PostersLoaderOptions : IValidatableObject
 
     public TimeSpan QueriesInterval { get; set; } = TimeSpan.FromSeconds(1);
 
-    public TimeSpan ScanInterval { get; set; } = TimeSpan.FromHours(2);
+    public TimeSpan ScanInterval { get; set; } = TimeSpan.FromDays(1);
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
@@ -19,9 +19,20 @@ public class PostersLoaderOptions : IValidatableObject
             yield return new ValidationResult("QueriesInterval must be positive", [nameof(QueriesInterval)]);
         }
 
+        if (QueriesInterval >= TimeSpan.FromSeconds(60))
+        {
+            yield return new ValidationResult("QueriesInterval must be less than 60 seconds", [nameof(QueriesInterval)]);
+        }
+
         if (ScanInterval <= TimeSpan.Zero)
         {
             yield return new ValidationResult("ScanInterval must be greater than zero", [nameof(ScanInterval)]);
+        }
+
+        if (ScanInterval.TotalMilliseconds > int.MaxValue)
+        {
+            // https://stackoverflow.com/questions/64553940/delaying-a-task-for-a-very-very-long-time
+            yield return new ValidationResult("ScanInterval.TotalMilliseconds must be less than int.MaxValue", [nameof(ScanInterval)]);
         }
     }
 }
