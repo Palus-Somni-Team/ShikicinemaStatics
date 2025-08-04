24b1b41d4f49068bab0f669b4a2c557f0511b1f3
Smarthard <th3smartchan@gmail.com>
2025-08-04 14:36:08 +0300

    convert only single file

--- scripts/convert-avif.sh
+++ scripts/convert-avif.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 #
-# Will convert all image files from specified directory to AVIF format
+# Will convert an image from specified path to AVIF format and store it near same directory
 #
 # HOW TO USE:
 #
-# ./convert-avif <path to static images directory> <file extension>
+# ./convert-avif <image path> <quality of convertion>
 #
 # DEPENDECIES:
 #
 # - ImageStick (https://imagemagick.org/script/magick.php)
 
-targetDir=${1?:'specify target directory path'}
-extension=${2:-jpeg}
+targetFile=${1}
+quality=${2:-50}
 
-for image in $targetDir/*.$extension; do
-        file=$(realpath "$image")
+[ -n "$targetFile" ] || { echo >&2 "specify image path to convert to AVIF"; exit 1; }
+[ -e "$targetFile" ] || { echo >&2 "file does not exists"; exit 2; }
+[ "$quality" -gt 0 ] || { echo >&2 "0 < quality ≤ 100"; exit 3; }
+[ "$quality" -le 100 ] || { echo >&2 "0 < quality ≤ 100"; exit 3; }
 
-        [ -e "$file" ] || continue
+file=$(realpath "$targetFile")
+dir=$(dirname "$file")
+extension="${file#*.}"
+name=$(basename ${file%.$extension})
 
-        animeId=$(basename ${file%.$extension})
-        convert "$file" -quality 20 -define avif:speed=10 "$targetDir/${animeId}.avif"
-        echo "'$file' -> '${animeId}.avif'"
-        sleep .5
-done
+convert "$file" -quality "$quality" -define avif:speed=10 "$dir/${name}.avif"
